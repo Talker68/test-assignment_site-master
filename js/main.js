c9b5ef91be9a87e10a5c08dacdc3e532bf18dc0d
iron-cherep <jacobxblue@gmail.com>
2017-01-30 03:55:59 +0300

    реализовал логику добавления новостей

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 var scrollButton = document.getElementsByClassName("sidebar__up-button")[0];
 var newsButton = document.getElementsByClassName("sidebar__news-button")[0];
+var newsList = document.getElementsByClassName("sidebar__item");
+var visibleNews = 9;
 
 window.onload = function() {
 
+  hideNews();
   scrollButton.addEventListener("click", scrollTop, false);
-
+  newsButton.addEventListener("click", addNews, false);
 };
 
 function scrollTop() {
-  console.log("scroool");
+
   var scrollStep = -window.scrollY / 20,
       scrollInterval = setInterval(function(){
       if ( window.scrollY != 0 ) {
@@ -18,6 +21,26 @@ function scrollTop() {
   },15);
 }
 
+/*
+Функция вызывается вначале и прячет лишние новости.
+Таким образом без js новости будут тоже доступны.
+*/
+function hideNews() {
+
+  for (var i=visibleNews; i < newsList.length; i++) {
+    newsList[i].classList.add("sidebar__item--hidden");
+  }
+  document.getElementsByClassName("sidebar")[0].classList.add("sidebar--limited-height");
+  document.getElementsByClassName("sidebar__navigation")[0].classList.add("sidebar__navigation--opened");
+
+}
+
 function addNews() {
 
+  document.getElementsByClassName("sidebar")[0].classList.remove("sidebar--limited-height");
+  document.getElementsByClassName("sidebar__navigation")[0].classList.remove("sidebar__navigation--opened");
+
+  for (var i=0; i < newsList.length; i++) {
+    newsList[i].classList.remove("sidebar__item--hidden");
+  }
 }
